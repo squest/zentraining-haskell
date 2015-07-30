151e417733e62aef9bbc863ae55628817c6b01f3
dsapoetra <angga.dimassaputra@gmail.com>
2015-07-30 19:05:26 +0700

    null done

--- dsapoetra/one.hs
+++ dsapoetra/one.hs
@@ -28,3 +28,43 @@ fak2 i = i * (fak2 $ pred i)
 
 sum' []=0
 sum' (x:xs) = x + (sum' xs)
+
+-- isPrime x
+  -- | x < 2 = False
+  -- | x == 2 = True
+  -- | even x = False
+  -- | otherwise = iter 3
+  -- where iter i
+    -- | (i*i) > x = True
+    -- | 0 == rem x i = False
+    -- | otherwise = iter $ i + 2
+
+-- fakIter i = iter 1 1
+  -- where iter n res
+    -- | n > i = res
+-- | otherwise = iter (n+1) (res*n)
+
+
+
+
+
+
+-- 1a- Reimplement :
+
+-- do not use any other function except : pred, succ, ++, :, pattern matching, |
+--pattern match use only (x:xs)/(x:[]). do not use (x:y:xs)
+
+-- null, take, drop, fst, snd, map, filter, delete, deleteAll, foldl, foldl1, zip, zipWith, (!!) -> ganti jadi nth,
+--csort, scanl, scanl1, elem, notElem, head, length, reverse, last, tail, init, max, min, concat, intersperse, intercalate, and, or, zip3, sum,
+--product, words, lines, unlines, unwords, takeWhile, dropWhile, concatMap, all, any, insert, zipWith3
+
+null' [] = True
+null'(x:xs) = False
+
+
+take' i [] = []
+take' i (x:xs)
+  | i <= 1 = [x]
+  | otherwise = ulang i
+  where ulang i
+  |
