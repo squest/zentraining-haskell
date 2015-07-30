c17a1244de9668ad71aa99e167963679972d50b5
Arifian Rahardianda <arifian.r@gmail.com>
2015-07-30 19:29:08 +0700

    upup

--- kambiing/kambiing.hs
+++ kambiing/kambiing.hs
@@ -2,35 +2,7 @@ module One where
 
 import Data.List
 
-x = 123
-
-sqr x = x*x
-cube x = x*x*x
-
-something = [324,34,345,456,56,7]
-
-cons x xs = x:xs
-
-mul3 = (3*)
-mul3a = (\x -> x * 3)
-
-mul3b x = 3*x
-
 abs' x = if x >= 0 then x else (- x)
-
-abs'' x
- | x >= 0 = x
- | otherwise = (- x)
-
-
-fak i = if i == 0 then 1 else i * (fak $ i - 1)
-
-fak1 i
-  | i == 0 = 1
-  | True = i * (fak1 $ pred i)
-
-fak2 0 = 1
-fak2 i = i * (fak2 $ pred i)
 
 isPrime n
   | n < 2 = False
