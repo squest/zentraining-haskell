e99772e4704d9bb4a3a33d1a2dfce24c0f4eb419
aisyahgunung <aisyahgunung@zeniuseducation.com>
2015-07-30 19:08:04 +0700

    sync

--- AWOL/one.hs
+++ AWOL/one.hs
@@ -15,4 +15,20 @@ fst' (x:xs) = x
 
 snd' (x:a:xs) = a
 
-map' 
+
+--map, filter, delete, nub, foldl, foldl1, zip, zipWith, (!!) -> ganti jadi nth,
+--sort, scanl, scanl1, inits, tails
+
+elem' _ [] = False
+elem' n (x:xs)
+  | x == n = True
+  | otherwise = elem' n xs
+
+
+notElem' _ [] = True
+notElem' n (x:xs)
+    | x == n = False
+    | otherwise = notElem' n xs
+
+
+--notElem, head, length, reverse, last, tail, init, max, min, minimum, maximum, concat, union, intersect, intersperse, intercalate, and, or, group, zip3, sum, product, splitAt, words, lines, unlines, unwords, takeWhile, dropWhile, concatMap, all, any, insert, partition, zipWith3
