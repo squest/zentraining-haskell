8eccee7340c6b51b54750d2e1d1cfb2ef48ef6eb
aisyahgunung <aisyahgunung@zeniuseducation.com>
2015-07-30 18:50:36 +0700

    sync

--- AWOL/one.hs
+++ AWOL/one.hs
@@ -1,3 +1,18 @@
 import Data.List
 
 null' [] = True
+null' _ = False
+
+take' _ [] = []
+take' 0 _ = []
+take' n (x:xs) = x : take' (pred n) xs
+
+drop' _ [] = []
+drop' 0 (x:xs) = x:xs
+drop' n (x:xs) = drop' (pred n) xs
+
+fst' (x:xs) = x
+
+snd' (x:a:xs) = a
+
+map' 
