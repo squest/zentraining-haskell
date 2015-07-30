f13d89dc30eaf3cc230a15495387359825ed6620
squest <sabdaps@gmail.com>
2015-07-30 15:43:37 +0700

    update session one

---
+++ tutorial/one.hs
@@ -0,0 +1,19 @@
+module One where
+
+import Data.List
+
+square x = x*x
+
+x = 123
+
+sqr x = x*x
+cube x = x*x*x
+
+something = [324,34,345,456,56,7]
+
+cons x xs = x:xs
+
+mul3 = (3*)
+mul3a = (\x -> x * 3)
+
+mul3b x = 3*x
