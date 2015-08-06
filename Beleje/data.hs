a32296c2a10bef799f90d9c65d786e37ad2cb79e
github-donnitazen <donna@zeniuseducation.com>
2015-08-06 21:57:52 +0700

    powerOf
    powerOf

--- Beleje/data.hs
+++ Beleje/data.hs
@@ -7,9 +7,14 @@ import One
 triangles = belScanl1 (+) [0..]
 
 -- PENTAGONAL
+pentagonals = 0:belScanl1 (+) [1,4..]
 
 -- FACTORIAL LIST
 factorials1 = belScanl1 (*) [1..]
 factorials2 = belMap (\a -> (belFoldl1 (*) [1..a])) [1..]
 
---
+-- FIBBO LIST
+fibbos = 0:1:belZipWith (+) fibbos (tail fibbos)
+
+-- POWER OF
+powerOf = (\a -> (belScanl1 (*) (cycle [a])))
