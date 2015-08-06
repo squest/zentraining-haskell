19a5a64f092b34a3595f4db035986422ea77c1f6
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-08-06 21:09:16 +0700

    sync

--- AWOL/one.hs
+++ AWOL/one.hs
@@ -26,7 +26,7 @@ delete' n (x:xs)
   | n == x = xs
   | otherwise = x : (delete' n xs)
 
--- nub, foldl, foldl1, zip, zipWith, (!!) -> ganti jadi nth,
+-- foldl, foldl1, zip, zipWith, (!!) -> ganti jadi nth,
 
 any' f [] = False
 any' f (x:xs) = if (f x) == True then True else any' f xs
@@ -35,12 +35,6 @@ sort' [] = []
 sort' (x:xs) = if (any' (\a -> a < x) xs) then sort' (xs ++ [x]) else [x] ++ sort' xs
 
 -- scanl, scanl1,
-
-inits' [] = [[]]
-inits' xs = inits' (init' xs) ++ [xs]
-
-tails' [] = [[]]
-tails' xs = xs : tails' (tail' xs)
 
 elem' _ [] = False
 elem' n (x:xs)
@@ -65,7 +59,7 @@ length' (x:xs) = 1 + length' xs
 
 init' [] = [] --butlast
 init' (x:xs)
-  | length (x:xs) == 1 = []
+  | length' (x:xs) == 1 = []
   | otherwise = [x] ++ init' xs
 
 tail' [] = [] --rest
@@ -79,6 +73,33 @@ min' a b
   | a < b = a
   | otherwise = b
 
+-- concat, intersperse, intercalate,
+
+and' [] = True
+and' (x:xs) = if x == True then and' xs else False
+
+or' [] = False
+or' (x:xs) = if x == True then True else or' xs
+
+-- zip3
+
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+-- words, lines, unlines, unwords, takeWhile, dropWhile, concatMap, all,
+
+insert' a [] = [a]
+insert' a (x:xs) = if a > x then (x:xs) ++ [a] else a : (x:xs)
+-- zipWith3
+
+-------------------------------------------------- 1b
+-------------------------------------------------- 1b
+
+  -- nub
+
 maximum' (x:y:xs)
   | xs == [] = if x > y then x else y
   | otherwise = maximum' (y:xs)
@@ -91,28 +112,28 @@ minimum' (x:y:xs)
   | xs == [] = if x < y then x else y
   | otherwise = minimum' (y:xs)
 
--- concat, union, intersect, intersperse, intercalate,
+inits' [] = [[]]
+inits' xs = inits' (init' xs) ++ [xs]
 
-and' [] = True
-and' (x:xs) = if x == True then and' xs else False
+tails' [] = [[]]
+tails' xs = xs : tails' (tail' xs)
 
-or' [] = False
-or' (x:xs) = if x == True then True else or' xs
-
-
--- group, zip3
-
-sum' [] = 0
-sum' (x:xs) = x + sum' xs
-
-product' [] = 1
-product' (x:xs) = x * product' xs
+-- union, intersect, group
 
 splitAt' _ [] = []
 splitAt' n x = take n x : [drop n x]
 
--- words, lines, unlines, unwords, takeWhile, dropWhile, concatMap, all,
+-- partition
 
-insert' a [] = [a]
-insert' a (x:xs) = if a > x then (x:xs) ++ [a] else a : (x:xs)
--- partition, zipWith3
+replicate' 0 _ = []
+replicate' 1 xs = [xs]
+replicate' n xs = xs : replicate' (pred n) xs
+
+-------------------------------------------------- 2 Lazies  (use take or you die)
+-------------------------------------------------- 2
+
+repeat' x = x : repeat' x
+
+cycle' x = x ++ cycle' x
+
+iterate' f x = x : iterate' f (f x)
