614d77beadb23d80308e435926c91c2556fcff3a
github-donnitazen <donna@zeniuseducation.com>
2015-08-06 21:13:57 +0700

    reset
    reset

--- Beleje/data.hs
+++ Beleje/data.hs
@@ -7,12 +7,5 @@ import One
 triangles = belScanl1 (+) [0..]
 
 -- PENTAGONAL
-pentagonals = belScanl1 (+) [1,4..]
 
---
-
-fak' 0 = 1
-fak' n = n * fak' (n-1)
-
-fakList' [] = []
-fakList' x = map fak' x
+-- FAKTORIAL LIST
