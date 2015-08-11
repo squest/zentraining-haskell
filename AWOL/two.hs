d5091d18d79dece76a0e5479d1b150ae732a887c
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-08-11 10:48:54 +0700

    sync

--- AWOL/two.hs
+++ AWOL/two.hs
@@ -1,13 +1,10 @@
+import Data.List
+
 --primes (infinite list of prime numbers, without using any function as described in the video)
 
 
 --fibos => [1,2,3,5..]
---fibos x
---  | x == 1 = [1]
---  | x == 2 = [1, 2]
---  | otherwise
-
-
+fibo = 1 : 2 : zipWith (+) fibo (tail fibo)
 
 --primorials
 
@@ -27,9 +24,21 @@ factorials x
 --pascal => [[1],[1,1],[1,2,1]...]
 
 --powerOf (infinite list of powerOf i)
+power a n
+  | n == 0 = 1
+  | n == 1 = a
+  | otherwise = a * power a (pred n)
+
+powerOf x = zipWith power (repeat x) [0..]
+
+--powerOf' x = zipWith (*) [1..] [1..] ++ zipWith (*) [1..] (tail powerOf' x)
+
+pow x = [x] ++ [x * (last (pow x))]
+
+--powerOf' x = foldl1 (*) (take x (repeat 1)) : --foldl1 (*) (take x (repeat 1))
 
 
---ex. powerOf 2 = [1,2,4,8,16,32...]
+--ex. powerOf 2 = [1,2,4,8,16,32...] power 2 n
 
 --sumPrimes => all possible summation of consecutive primes
 
