3233e9b165b4f02b48a8fc267b7f2e17d2fc74fa
squest <sabdaps@gmail.com>
2015-08-13 17:55:11 +0700

    ude tuh
    @donnitazen @calvinirwan @zulfikarhhh @arifian , ya lengkapin sendiri
    lah yang laennya

--- tutorial/ans.hs
+++ tutorial/ans.hs
@@ -8,6 +8,8 @@ oddPrime n = all (\x -> 0 /= rem n x) $ takeWhile (\x -> x*x <= n) [3,5..]
 
 primes = 2 : filter oddPrime [3,5..]
 
+primes' = 2:filter (\x -> all (\i -> 0 /= rem x i) (takeWhile (\k -> k*k <= x) primes')) [3,5..]
+
 primorials = scanl1 (*) primes
 
 fibo = 1:2:zipWith (+) fibo (tail fibo)
@@ -19,6 +21,11 @@ squares = map (\x -> x*x) [1..]
 power2 = iterate (*2) 1
 
 triangles = scanl1 (+) [1..]
+squares = scanl1 (+) [1,3..]
+pentals = scanl1 (+) [1,4..]
+
+powerOf = (!!) (map (\x -> iterate (*x) 1) [0..])
+
 
 
 
